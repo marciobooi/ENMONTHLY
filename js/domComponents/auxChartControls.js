a8e207f45aa3824fa6a180aac2e7e5072e5a3331
marcio <78marcio@gmail.com>
2026-01-16 22:51:59 +0100

    feat: update subtitle heading level for improved semantic structure

--- js/domComponents/auxChartControls.js
+++ js/domComponents/auxChartControls.js
@@ -13,7 +13,7 @@ class ChartControls {
 			<div class="container-fluid">
 			  <div id="auxChartTitle">
 				<h2 id="title" class="title auxTitle"></h2>
-				<h6 id="subtitle" class="subtitle auxSubtitle"></h6>
+					<h3 id="subtitle" class="subtitle auxSubtitle"></h3>>
 			  </div>
 			  <div class="menu">
 				<ul id="chartBtns"  aria-label="options graph toolbox" class="navbar-nav ms-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 50vw;">
